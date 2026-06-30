c7cb2600f358a356dbd43b829b1e8bd28241edda
ganapurba007 <ganapurbakusuma86@gmail.com>
2026-06-30 14:24:50 +0700

    add logout API

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,19 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\api\RegisterController;
 use App\Http\Controllers\api\LoginController;
+use App\Http\Controllers\api\LogoutController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/logout', LogoutController::class);
+});
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
 
 Route::post('/register', RegisterController::class);
 Route::post('/login', LoginController::class);
